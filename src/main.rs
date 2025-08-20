30c6f74d845ece9e8a39b56b7563d3cacc37aa4b
Maxwell Rupp <maxwell@giglab.dev>
2025-08-20 01:57:42 -0400

    got global signal working for music test

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use dioxus::prelude::*;
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 // const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
+static SONG: GlobalSignal<String> = Signal::global(|| "Drift Away".to_string());
+
 fn main() {
     dioxus::launch(App);
 }
@@ -17,13 +19,35 @@ fn main() {
 #[derive(Clone)]
 struct TitleState(String);
 
+#[derive(Clone, Copy)]
+struct MusicPlayer {
+    song: Signal<String>,
+}
+
+fn use_music_player_provider() {
+    let song = use_signal(|| "Drift Away".to_string());
+    use_context_provider(|| MusicPlayer { song });
+}
+
 #[component]
 fn App() -> Element {
     use_context_provider(|| TitleState("HotDawg! 🌭".to_string()));
+    use_music_player_provider();
     rsx! {
         document::Stylesheet{href: MAIN_CSS}
         Title {}
+        Player {}
         DogView {}
+    }
+}
+#[component]
+fn Player() -> Element {
+    rsx! {
+        h3 {"Now Playing {SONG}"}
+        button {
+            onclick: move |_| *SONG.write() = "Vienna".to_string(),
+            "Shuffle"
+        }
     }
 }
 
