29e4d8f94b3f9f8603b096da0484da76a467a169
Maxwell Rupp <maxwell@giglab.dev>
2025-08-20 22:34:03 -0400

    TMP-NOT-WORKING
    mid work on https://dioxuslabs.com/learn/0.6/guide/routing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use dioxus::prelude::*;
 
 mod backend;
-mod frontend;
+mod components;
+
+use crate::components::*;
 
 // const FAVICON: Asset = asset!("/assets/favicon.ico");
 // const HEADER_SVG: Asset = asset!("/assets/header.svg");
