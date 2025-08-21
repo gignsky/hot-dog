e3e50b6c87cdd2f51013d627dcb38fc588638539
Maxwell Rupp <maxwell@giglab.dev>
2025-08-21 09:08:09 -0400

    removed splash file

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,7 @@
 pub mod favorites;
 pub mod nav;
-pub mod splash;
 pub mod view;
 
 pub use favorites::*;
 pub use nav::*;
-pub use splash::*;
 pub use view::*;
--- src/components/splash.rs
+++
@@ -1,2 +0,0 @@
-// TODO: This is a test comment
-//
