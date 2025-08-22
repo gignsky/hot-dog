ff8d8fa3dd76b2818c25ea60d6fe2c3d2ad689f1
Maxwell Rupp <maxwell@giglab.dev>
2025-08-22 00:37:17 -0400

    reverted to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
